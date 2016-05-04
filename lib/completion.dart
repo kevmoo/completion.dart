5f06161f957ab2fea2c7cc419ddf8e776dd63971
Kevin Moore <kevmoo@google.com>
2016-05-04 12:27:36 -0700

    Expose generateCompletionScript as a callable function in completion lib

--- lib/completion.dart
+++ lib/completion.dart
@@ -3,6 +3,8 @@ import 'package:args/args.dart';
 import 'src/get_args_completions.dart';
 import 'src/try_completion.dart';
 
+export 'src/generate.dart';
+
 ArgResults tryArgsCompletion(List<String> mainArgs, ArgParser parser) {
   tryCompletion(mainArgs, (List<String> args, String compLine, int compPoint) {
     return getArgsCompletions(parser, args, compLine, compPoint);
