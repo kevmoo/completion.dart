94e5f4e5bfdbf8c42efe303c9140a9ca5fece6ff
Kevin Moore <kevmoo@google.com>
2019-05-22 12:33:09 -0700

    fix dart 2.0.0 travis

--- lib/completion.dart
+++ lib/completion.dart
@@ -16,7 +16,7 @@ ArgResults tryArgsCompletion(
     (List<String> args, String compLine, int compPoint) {
       return getArgsCompletions(parser, args, compLine, compPoint);
     },
-    // ignore: deprecated_member_use_from_same_package
+    // ignore: deprecated_member_use_from_same_package,deprecated_member_use
     logFile: logFile,
   );
   return parser.parse(mainArgs);
