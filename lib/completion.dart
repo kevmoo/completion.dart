eb7286f9c76be2dabfee508139d189f15ce4b310
Kevin Moore <kevmoo@google.com>
2026-02-18 22:24:47 -0800

    small doc tweaks

--- lib/completion.dart
+++ lib/completion.dart
@@ -1,4 +1,7 @@
 /// Support for generating shell completion scripts and using them.
+///
+/// Most users will want to use the [tryArgsCompletion] function.
+/// @docImport 'src/try_args_completion.dart';
 library;
 
 export 'src/generate.dart' show Shell, generateCompletionScript;
